b103eb22c04019fd1d70dcf2862b736581b7f14e
Aadi Poddar <aadipoddarmail@gmail.com>
2023-12-23 15:36:36 +0530

    Plate UI Icons

---
+++ Assets/Scripts/PlateIconsUI.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PlateIconsUI : MonoBehaviour
+{
+    [SerializeField] private PlateKitchenObject plateKitchenObject;
+    [SerializeField] private Transform iconTemplate;
+
+    private void Awake()
+    {
+        iconTemplate.gameObject.SetActive(false);
+    }
+
+    private void Start()
+    {
+        plateKitchenObject.OnTngredientAdded += PlateKitchenObject_OnTngredientAdded;
+    }
+
+    private void PlateKitchenObject_OnTngredientAdded(object sender, PlateKitchenObject.OnTngredientAddedEventArgs e)
+    {
+        UpdateVisual();
+    }
+
+    private void UpdateVisual()
+    {
+        foreach (Transform child in transform)
+        {
+            if (child == iconTemplate) continue;
+            Destroy(child.gameObject);
+        }
+
+        foreach (KitchenObjectSO kitchenObjectSO in plateKitchenObject.GetKitchenObjectSOList())
+        {
+            Transform iconTransform = Instantiate(iconTemplate, transform);
+            iconTransform.gameObject.SetActive(true);
+            iconTransform.GetComponent<PlateIconsSingleUI>().SetKitchenObjectSO(kitchenObjectSO);
+        }
+    }
+}
