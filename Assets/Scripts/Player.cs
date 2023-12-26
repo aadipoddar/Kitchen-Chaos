c7d6558c93d73ea3b4bec6698cc10980178916e8
Aadi Poddar <aadipoddarmail@gmail.com>
2023-12-27 00:14:01 +0530

    Sync Player Movement and Animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -57,6 +57,11 @@ public class Player : NetworkBehaviour, IKitchenObjectParent
 
     private void Update()
     {
+        if (!IsOwner)
+        {
+            return;
+        }
+
         HandleMovement();
         HandleInteractions();
     }
